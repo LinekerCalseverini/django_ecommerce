1331a80580b310b9521cb93a591988ebfcf6dbd4
LinekerCalseverini <lineker.calseverini@gmail.com>
2024-01-28 12:31:57 -0300

    Projeto e-commerce - Adicionar ao carrinho (Parte 2)

--- produto/views.py
+++ produto/views.py
@@ -1,4 +1,5 @@
 from typing import Any
+from django.db.models.query import QuerySet
 from django.shortcuts import redirect, reverse, get_object_or_404
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
@@ -63,12 +64,23 @@ class AdicionarAoCarrinho(View):
 
         quantidade = carrinho.get(variacao_id, 0)
         quantidade += 1
+        if variacao.estoque < quantidade:
+            messages.error(
+                self.request,
+                'Estoque insuficiente.'
+            )
+            return redirect(http_referer)
         carrinho[variacao_id] = quantidade
 
         self.request.session['carrinho'] = carrinho
         self.request.session.save()
 
-        return HttpResponse(variacao)
+        messages.success(
+            self.request,
+            'Produto adicionado com sucesso.'
+        )
+
+        return redirect(http_referer)
 
 
 class RemoverDoCarrinho(View):
@@ -76,9 +88,15 @@ class RemoverDoCarrinho(View):
         return HttpResponse('RemoverDoCarrinho')
 
 
-class Carrinho(View):
-    def get(self, *args, **kwargs):
-        return HttpResponse('Carrinho')
+class Carrinho(ListView):
+    template_name = 'produto/carrinho.html'
+    context_object_name = 'itens'
+
+    def get_queryset(self) -> QuerySet[Any]:
+        variacao_ids = self.request.session.get('carrinho', {}).keys()
+        query_set = Variacao.objects.filter(id__in=variacao_ids)
+
+        return query_set
 
 
 class Finalizar(View):
